bc4624bf5837ca7ab70339a4c500ec607178ebbc
ras0q <ras0q@users.noreply.github.com>
2024-08-31 01:45:31 +0900

    :bug: cut left side of overflow box

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -101,6 +102,8 @@ func main() {
 	copy(bgLines, originalBgLines)
 
 	for i, loc := range matchLocs {
+		log.Debug("display popup", "i", i, "loc", loc, "match", wrapped[loc[0]:loc[1]])
+
 		boxLines := strings.Split(
 			boxStyle.Render(fmt.Sprintf("%s", wrapped[loc[0]:loc[1]])),
 			"\n",
@@ -117,7 +120,20 @@ func main() {
 			sum += len(line + "\n")
 		}
 
+		log.Debug("position", "row", row, "col", col)
+		if *isDebug {
+			actual := originalBgLines[row+boxVPad][col+boxHPad : col+boxHPad+loc[1]-loc[0]]
+			expected := wrapped[loc[0]:loc[1]]
+			if expected != actual {
+				log.Error("incorrect position", "expected", expected, "actual", actual)
+			}
+		}
+
 		for j := range len(boxLines) {
+			if row+j < 0 {
+				continue
+			}
+
 			if l := row + j + len(boxLines) - len(bgLines); l > 0 {
 				bgLines = append(
 					bgLines,
@@ -131,23 +147,25 @@ func main() {
 				bgLine += strings.Repeat(" ", col-sw)
 			}
 
-			newBgLine := ansi.Truncate(bgLine, col, "") + boxLine
+			bgLeft := ansi.Truncate(bgLine, col, "")
 
-			// NOTE: bgLine has no newline, so [strings.Join] after [strings.Split] is safe.
-			wrapped := strings.Split(
-				ansi.Hardwrap(bgLine, col+ansi.StringWidth(boxLine), true),
-				"\n",
-			)
-			if len(wrapped) >= 2 {
-				var ansiStyle string
-				ansiStyles := ansiStyleRegexp.FindAllString(wrapped[0], -1)
-				if l := len(ansiStyles); l > 0 {
-					ansiStyle = ansiStyles[l-1]
+			if col < 0 {
+				_boxLine, err := cutLeft(boxLine, -col)
+				if err != nil {
+					log.Error("cut left of boxLine", "err", err)
 				}
-				newBgLine += ansiStyle + strings.Join(wrapped[1:], "")
+
+				log.Debug("boxLine", "boxLine", _boxLine)
+
+				boxLine = _boxLine
 			}
 
-			bgLines[row+j] = ansi.Truncate(newBgLine, w, "")
+			bgRight, err := cutLeft(bgLine, max(0, col)+ansi.StringWidth(boxLine))
+			if err != nil {
+				log.Error("cut left of bgLine", "err", err)
+			}
+
+			bgLines[row+j] = ansi.Truncate(bgLeft+boxLine+bgRight, w, "")
 		}
 
 		start, end := 0, len(bgLines)
@@ -156,14 +174,36 @@ func main() {
 			start = max(0, row-padding)
 			end = min(len(bgLines), row-padding+h)
 		}
-		fmt.Println(
-			ansi.EraseEntireLine +
-				strings.Join(bgLines[start:end], "\n"+ansi.EraseEntireLine),
-		)
+
+		var prefix string
+		if !*isDebug {
+			prefix = ansi.EraseEntireLine
+		}
+		fmt.Println(prefix + strings.Join(bgLines[start:end], "\n"+prefix))
 		time.Sleep(time.Millisecond * time.Duration(*sleep))
 
 		if i != len(matchLocs)-1 && !*isDebug {
 			fmt.Print(ansi.CursorPreviousLine(end - start))
 		}
 	}
+}
+
+func cutLeft(line string, padding int) (string, error) {
+	if strings.Contains(line, "\n") {
+		return "", errors.New("line must not contain newline")
+	}
+
+	// NOTE: line has no newline, so [strings.Join] after [strings.Split] is safe.
+	wrapped := strings.Split(ansi.Hardwrap(line, padding, true), "\n")
+	if len(wrapped) == 1 {
+		return wrapped[0], nil
+	}
+
+	var ansiStyle string
+	ansiStyles := ansiStyleRegexp.FindAllString(wrapped[0], -1)
+	if l := len(ansiStyles); l > 0 {
+		ansiStyle = ansiStyles[l-1]
+	}
+
+	return ansiStyle + strings.Join(wrapped[1:], ""), nil
 }
